93e1fe5165a3019a69b26fade4c7fc607e3b91ab
Vignesh Sivarajah <vignesh.sivarajah@systek.no>
2021-06-19 14:42:45 +0200

    PART 1- REST API interface

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/vsivarajah/distributed_services/internal/server"
+	"log"
+)
+
+func main(){
+	srv := server.NewHTTPServer(":8080")
+	log.Fatal(srv.ListenAndServe())
+}
+
