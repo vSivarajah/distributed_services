6da9f8f0e5125d8606fa3b3ce3bd77e205af8acf
Vignesh Sivarajah <vignesh.sivarajah@systek.no>
2021-06-21 13:19:54 +0200

    added store logic

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/vsivarajah/distributed_services
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ internal/log/store.go
@@ -0,0 +1,108 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu sync.Mutex
+	buf *bufio.Writer
+	size uint64
+}
+
+//newStore creates a store for the given file.
+func newStore(f *os.File) (*store, error)  {
+	//fetch file info and store file name in fi
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	//fetch file size and store it in size variable
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		buf:  bufio.NewWriter(f),
+		size: size,
+	}, nil
+}
+
+//Append persists the given bytes to the store.
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	//write the length of the record. When reading the record, we know amount of bytes to read.
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	//write to the buffered writer instead of directly to the file
+	//to reduce number of system calls and improve performance
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	w += lenWidth
+	s.size += uint64(w)
+
+	//return number of bytes return and position where the store holds the record in its file.
+	//segment will use the position when creating an associatied index entry for this record.
+	return uint64(w), pos, nil
+}
+
+
+//Read returns the record stored at the given position.
+func (s *store) Read(pos uint64) ([]byte, error)  {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	//flushes the writer buffer, in case trying to read a record that the buffer hasn't flushed to disk yet.
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+
+}
+
+//ReadAt reads bytes into p beginning at the offset in the store's file.
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+//Close persists any buffered data before closing the file
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
+}
+
