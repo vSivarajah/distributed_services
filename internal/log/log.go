cef62514fa097f302532743b6b6d8a6e6ee0e5e3
Vignesh Sivarajah <vignesh.sivarajah@systek.no>
2021-07-05 13:02:32 +0200

    added grpc server and client

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"fmt"
 	api "github.com/vsivarajah/distributed_services/api/v1"
 	"io"
 	"io/ioutil"
@@ -115,7 +114,7 @@ func (l *Log) Read(off uint64) (*api.Record, error) {
 		}
 	}
 	if s == nil || s.nextOffset <= off {
-		return nil, fmt.Errorf("offset out of range: %d", off)
+		return nil, api.ErrOffsetOutOfRange{Offset: off}
 	}
 	return s.Read(off)
 
---
+++ internal/server/server.go
@@ -0,0 +1,102 @@
+package server
+
+import (
+	"context"
+	api "github.com/vsivarajah/distributed_services/api/v1"
+	"google.golang.org/grpc"
+)
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+func NewGRPCServer (config *Config) (*grpc.Server, error) {
+	gsrv := grpc.NewServer()
+	srv, err := newgrpcServer(config)
+	if err != nil {
+		return nil, err
+	}
+	api.RegisterLogServer(gsrv, srv)
+	return gsrv, nil
+}
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error){
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+//ProduceStream implements a bidirectional streaming RPC so the client can stream data into the server's log
+//and the server can tell the client whether each request succeeded.
+func(s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer,
+	) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+//ConsumeStream implements a server-side streaming RPC so the client cant tell the server where in the log to read records
+//and then the server will stream every record that follows -- even the records that aren't in the log yet.
+//when the server reaches the end of the log, the server will wait until someone appends a record to the log and then continue
+//streaming records to the client
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer,
+	) error {
+	for {
+		select {
+		case <- stream.Context().Done():
+				return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
